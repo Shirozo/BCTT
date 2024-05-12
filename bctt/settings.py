a250915d9b3e83c812cc4aa45bd6a51355639514
Shirozo <caranzoneilbryan@gmail.com>
2024-05-13 01:19:14 +0800

    Add django admin

--- bctt/settings.py
+++ bctt/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
 
-    'admin.apps.AdminConfig',
+    'account.apps.AccountConfig',
     'drivers.apps.DriversConfig',
     'transaction.apps.TransactionConfig'
 ]
--- bctt/urls.py
+++ bctt/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("admin.urls")),
+    path("admin", admin.site.urls),
     path("driver/", include("drivers.urls")),
-    path("transaction/", include("transaction.urls"))
+    path("", include("transaction.urls"))
 ]
