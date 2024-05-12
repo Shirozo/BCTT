a1498b055cb5101076cbcbf367297b8d45f453fe
Shirozo <caranzoneilbryan@gmail.com>
2024-05-13 01:07:20 +0800

    one and only class

---
+++ transaction/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from drivers.forms import FormSetting
+from .models import Transaction
+
+class TransactionForm(FormSetting):
+    class Meta:
+        model = Transaction
+        fields = [
+            "driver",
+            "amount"
+        ]
