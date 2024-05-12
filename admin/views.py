61cd65a7d1dcf8e10f6bd465d6fbca9d0f2c2258
Shirozo <caranzoneilbryan@gmail.com>
2024-05-13 01:09:33 +0800

    login now

--- admin/views.py
+++ admin/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
 def home(request):
-    return render(request, "index.html")
+    return redirect(reverse("reload"))
