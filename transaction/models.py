a48dce4016f49eba36f23bb581257cfdb3340e07
Shirozo <caranzoneilbryan@gmail.com>
2024-05-13 01:07:49 +0800

    2 models

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from drivers.models import Driver
 
 # Create your models here.
+class Transaction(models.Model):
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=False)
+    amount = models.PositiveIntegerField(default=1)
+    transact_date = models.DateTimeField(auto_now=True)
+
+
+class DriverScan(models.Model):
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=False)
+    amount = models.PositiveIntegerField(default=0)
+    scanDate = models.DateTimeField(auto_now=True)
