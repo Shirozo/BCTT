169b156c8431d99539edcf700b8659eb6bce0aea
Shirozo <caranzoneilbryan@gmail.com>
2024-05-13 22:14:02 +0800

    Added driver-operator link FK

--- drivers/models.py
+++ drivers/models.py
@@ -19,6 +19,7 @@ class Driver(models.Model):
         return f"{self.last_name}, {self.first_name}"
     
 class Operator(models.Model):
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=True, blank=True)
     first_name = models.CharField(max_length=50, null=False)
     last_name = models.CharField(max_length=50, null=False)
     address = models.CharField(max_length=100, null=False)
