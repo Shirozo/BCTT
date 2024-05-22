9a3eefbc4d1ec8b9d441fc7a8bf25542070d4e38
Shirozo <caranzoneilbryan@gmail.com>
2024-05-22 21:49:55 +0800

    Final

--- drivers/models.py
+++ drivers/models.py
@@ -11,15 +11,12 @@ class Operator(models.Model):
         return f"{self.operator_last_name}, {self.operator_first_name}"
 
 class Driver(models.Model):
-    class VehicleIdentity(models.TextChoices):
-        Cab = "1", "Cab"
-        Tricycle = "2", "Tricycle"
 
     first_name = models.CharField(max_length=50, null=False)
     last_name = models.CharField(max_length=50, null=False)
     qr_code = models.CharField(max_length=100)
     plate_number = models.CharField(max_length=10, null=False, unique=True)
-    vhs = models.CharField(choices=VehicleIdentity.choices, null=True, blank=True, max_length=10)
+    vhs = models.IntegerField(default=1)
     rate = models.PositiveIntegerField(default=1)
     balance = models.PositiveIntegerField(default=0)
     operator = models.ForeignKey(Operator, on_delete=models.CASCADE, null=True, blank=True)
