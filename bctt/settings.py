c766ad9e107d78a23e893269ba9ff82e15b3e3fc
Shirozo <caranzoneilbryan@gmail.com>
2024-05-22 21:58:04 +0800

    static

--- bctt/settings.py
+++ bctt/settings.py
@@ -131,10 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     join(BASE_DIR, 'static')
-]
-
+)
 LOGIN_URL = '/account/login'
 
 # Default primary key field type
