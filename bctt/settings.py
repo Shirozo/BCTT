3e24e8c6fbcb5e1cdeb7c74596ab44eba53a06ff
Shirozo <caranzoneilbryan@gmail.com>
2024-05-22 22:24:36 +0800

    static

--- bctt/settings.py
+++ bctt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k4t989&j13qau!(sgknt(6nf!e511&xsh2%h24mu)y!=v6g_p-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*", ".vercel.app"]
 
