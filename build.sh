509a0f9eaddae520b62820e59324d1c0937d4b4f
Shirozo <caranzoneilbryan@gmail.com>
2024-05-23 09:53:46 +0800

    sana

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-python3.9 manage.py collectstatic --noinput --clear  
+python3.9 manage.py collectstatic
