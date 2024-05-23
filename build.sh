d375f891a3c3d9edc844a7c554ca9d4e309c3b84
Shirozo <caranzoneilbryan@gmail.com>
2024-05-23 11:08:48 +0800

    css

--- build.sh
+++ build.sh
@@ -5,11 +5,5 @@ python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
-set -e
-
 python3 manage.py collectstatic
 
-echo "Creating superuser..."
-python manage.py createsuperuser --no-input --username 'admin' --email '1@1.com' <<EOF
-'admin'
-EOF
