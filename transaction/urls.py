7b4779f6291400decfadb031b3655a8afc266018
Shirozo <caranzoneilbryan@gmail.com>
2024-06-10 22:03:56 +0800

    Reload url not used

--- transaction/urls.py
+++ transaction/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", view=views.transactions, name="transac"),
-    path("reload/", view=views.reload, name="reload"),
-    path("scan/", view=views.scanQr, name="scan")
+    # path("reload/", view=views.reload, name="reload"),
+    path("scan", view=views.scanQr, name="scan"),
 ]
