3190aa23e59238c1598cacf780530081af657de3
Shirozo <caranzoneilbryan@gmail.com>
2024-06-10 22:03:34 +0800

    Removed amount and add action

--- transaction/models.py
+++ transaction/models.py
@@ -4,12 +4,11 @@ from drivers.models import Driver
 # Create your models here.
 class Transaction(models.Model):
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=False)
-    amount = models.PositiveIntegerField(default=1)
     transact_date = models.DateTimeField(auto_now=True)
 
 
 class DriverScan(models.Model):
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE, null=False)
-    amount = models.PositiveIntegerField(default=0)
-    endpoint = models.CharField(max_length=50)
+    endpoint = models.CharField(max_length=50, null=True)
     scanDate = models.DateTimeField(auto_now=True)
+    action = models.CharField(max_length=10, null=True, blank=True)
