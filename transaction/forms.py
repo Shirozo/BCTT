d35cf12c1ef642764fccfb14676b1d8a1b86e309
Shirozo <caranzoneilbryan@gmail.com>
2024-06-10 22:03:07 +0800

    Not used

--- transaction/forms.py
+++ transaction/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from drivers.forms import FormSetting
 from .models import Transaction
 
-class TransactionForm(FormSetting):
-    class Meta:
-        model = Transaction
-        fields = [
-            "driver",
-            "amount"
-        ]
+# class TransactionForm(FormSetting):
+#     class Meta:
+#         model = Transaction
+#         fields = [
+#             "driver",
+#             "amount"
+#         ]
