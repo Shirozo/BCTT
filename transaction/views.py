e4516bee608f7712f882d95afe3d1fd4d86ae51d
Shirozo <caranzoneilbryan@gmail.com>
2024-06-10 22:04:14 +0800

    Reload url not used and chnages handler

--- transaction/views.py
+++ transaction/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse
-from .forms import TransactionForm
+# from .forms import TransactionForm-
 from django.contrib import messages
 from drivers.models import Driver
 from .models import Transaction, DriverScan
@@ -8,39 +8,40 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 # Create your views here.
 
-@login_required
-def reload(request):
-    context = {}
-    if request.method == "POST":
-        form = TransactionForm(request.POST)
-        if form.is_valid():
-            data = form.save()
-            id = data.driver.id
+# @login_required
+# def reload(request):
+#     context = {}
+#     if request.method == "POST":
+#         form = TransactionForm(request.POST)
+#         if form.is_valid():
+#             data = form.save()
+#             id = data.driver.id
 
-            past_balance = Driver.objects.get(id=id).balance
+#             past_balance = Driver.objects.get(id=id).balance
             
-            Driver.objects.filter(id=id).update(
-                balance = past_balance + data.amount
-            )
+#             Driver.objects.filter(id=id).update(
+#                 balance = past_balance + data.amount
+#             )
 
-            messages.success(request, f"Added Balance to {data.driver}!")
-        else:
-            messages.error(request, "Invalid Form!")
-    else:
-        reload_form = TransactionForm()
-        context['reload_form'] = reload_form
+#             messages.success(request, f"Added Balance to {data.driver}!")
+#         else:
+#             messages.error(request, "Invalid Form!")
+#     else:
+#         reload_form = TransactionForm()
+#         context['reload_form'] = reload_form
     
-    transact_hstry = Transaction.objects.all()
-    context['hstry'] = transact_hstry
-    return render(request, "reload.html", context)
+#     transact_hstry = Transaction.objects.all()
+#     context['hstry'] = transact_hstry
+#     return render(request, "reload.html", context)
 
 
 @login_required
 def scanQr(request):
     context = {}
     data = request.GET.get("data")
+    action = request.GET.get("action")
     try:
-        id, plate_number, rate = data.split("-")
+        id, plate_number, _ = data.split("-")
         endpoint = request.GET.get("endpoint")
     except Exception:
         context['code'] = 500
@@ -61,19 +62,40 @@ def scanQr(request):
                 
                 driver_data = has_data[0]
 
-                if driver_data.balance >= int(rate):
-                    transact_detail = DriverScan(driver=driver_data, amount=rate, endpoint=endpoint)
-                    transact_detail.save()
+                if action.strip() == "pay":
+                    if driver_data.status == "unpaid":
+                        transact_detail = DriverScan(driver=driver_data, action="Pay")
+                        transact_detail.save()
 
-                    has_data.update(balance = driver_data.balance - int(rate))
-                    context['code'] = 200
-                    context['plate_number'] = driver_data.plate_number
-                    context['message'] = "Transaction Success!"
-                    messages.success(request, "Transaction Success!")
+                        has_data.update(status = "paid")
+                        context['code'] = 200
+                        context['plate_number'] = driver_data.plate_number
+                        context['message'] = "Transaction Success!"
+                        messages.success(request, "Transaction Success!")
+                    else:
+                        context['code'] = 403
+                        context['message'] = "Already scanned!"
+                        messages.error(request, "Already scanned!")
+                
+                elif action.strip() == "pass":
+                    if driver_data.status == "paid":
+                        transact_detail = DriverScan(driver=driver_data, endpoint=endpoint, action="Scanned")
+                        transact_detail.save()
+
+                        has_data.update(status = "unpaid")
+                        context['code'] = 200
+                        context['plate_number'] = driver_data.plate_number
+                        context['message'] = "Transaction Success!"
+                        messages.success(request, "Transaction Success!")
+                    else:
+                        context['code'] = 403
+                        context['message'] = "Driver didn't pay!"
+                        messages.error(request, "Driver didn't pay!")
+                
                 else:
-                    context['code'] = 403
-                    context['message'] = "Insufficient Balance!"
-                    messages.error(request, "Insufficient Balance!")
+                    context['code'] = 404
+                    context["message"] = "Driver Details not Found"
+                    messages.error(request, "Driver Details not Found")
 
             else:
                 context['code'] = 404
@@ -86,10 +108,8 @@ def scanQr(request):
 @login_required
 def transactions(request):
     transact = DriverScan.objects.all()
-    total = transact.aggregate(total=Sum('amount'))['total']
     context = {
         "transact" : transact,
         "count" : transact.count(),
-        "total" : total
     }
     return render(request, "transaction.html", context)
